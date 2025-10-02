e16de4fbd8d9fce36a60d4bcdc48429b9f731c7d
Vojta Vorel <vorelvo2@uhk.cz>
2025-10-02 12:15:28 +0200

    Po cviku 2

--- src/main/java/palg/Cviko02_Uloha02.java
+++ src/main/java/palg/Cviko02_Uloha02.java
@@ -1,17 +1,31 @@
 package palg;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Cviko02_Uloha02
 {
     // Doplň metodu sumOfSmall (se dvěma vstupy), která vrátí součet čísel v daném poli,
     // která jsou nižší než daný limit.
     // Ze souboru southMoravia.txt zjisti, kolik lidí žije v obcích s méně než 1000 obyvatel.
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
+        var ints
+                = Cviko02_FileUtils.integersFromFile("southMoravia.txt");
+        System.out.println(sumOfSmall(ints,1000));
     }
 
-    //public static ... sumOfSmall( ... , ... )
-    //{
-
-    //}
+    public static int sumOfSmall( int[] pole , int limit )
+    {
+        //return Arrays.stream(pole).filter(p->p<limit).sum();
+        int sum=0;
+        for(int i=0;i<pole.length;i++)
+        {
+            if(pole[i]<limit)
+            {
+                sum = sum+pole[i];
+            }
+        }
+        return sum;
+    }
 }
--- src/main/java/palg/Cviko02_Uloha03.java
+++ src/main/java/palg/Cviko02_Uloha03.java
@@ -1,16 +1,32 @@
 package palg;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Cviko02_Uloha03
 {
     // Chytré hodinky zaznamenávají každý den počet hodin strávených sportem (smartSportData.txt).
     // Doplň metodu maxZeroIntervalLength, která zjistí, kolik nejvýše dní za sebou bylo zaznamenáno zcela beze sportu.
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
+        int res= maxZeroIntervalLength(Cviko02_FileUtils.integersFromFile("smartSportData.txt"));
+        System.out.println(res);
     }
 
-//    public static ... maxZeroIntervalLength( int[] values )
-//    {
-//        return 0;
-//    }
+    public static int maxZeroIntervalLength( int[] values )
+    {
+        int best = 0;
+        int currentZerosCount = 0;
+        for (int i=0;i<values.length;i++)
+        {
+            if(values[i] == 0)
+                currentZerosCount++;
+            else
+                currentZerosCount=0;
+
+            if(currentZerosCount > best)
+                best = currentZerosCount;
+        }
+        return best;
+    }
 }
