557cc6dd4da91ddcfaed5d185adf082ce3da7ebc
Vojta Vorel <vorelvo2@uhk.cz>
2025-10-23 12:14:02 +0200

    Po cviku 5

--- src/main/java/palg/Cviko05_Uloha02/Main.java
+++ src/main/java/palg/Cviko05_Uloha02/Main.java
@@ -1,12 +1,20 @@
 package palg.Cviko05_Uloha02;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args) throws IOException
     {
+        Scanner s = new Scanner(System.in);
         // 1) Přidej třídy Queue a Stack implementující rozhraní Waiting
+        Waiting storage;
+        if(s.nextLine().equals("QUEUE"))
+            storage = new Queue();
+        else
+            storage = new Stack();
+        storage.add("AHOJ");
 
         // 2) Na jedné z nich otestuj, jaký je průměrný počet čekajících,
         //   pokud 100_000 krát provedeme add nebo remove s pravděpodobností 1:1
---
+++ src/main/java/palg/Cviko05_Uloha02/Queue.java
@@ -0,0 +1,27 @@
+package palg.Cviko05_Uloha02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queue implements Waiting {
+    List<Object> list = new ArrayList<>();
+
+    public void add(Object object) {
+        list.add(index(),object);
+    }
+    private int index()
+    {
+        return 0;
+    }
+
+    public Object remove() {
+        if(list.isEmpty())
+            return null;
+        Object result = list.removeLast();
+        return result;
+    }
+
+    public int count() {
+        return list.size();
+    }
+}
