b4546d7b56a752c141fea818b657a03503c2e08f
Vojta Vorel <vorelvo2@uhk.cz>
2025-10-30 11:50:00 +0100

    Během cvika 6

--- src/main/java/palg/Cviko06/RouteList.java
+++ src/main/java/palg/Cviko06/RouteList.java
@@ -1,5 +1,7 @@
 package palg.Cviko06;
 
+import java.util.List;
+
 public class RouteList
 {
     RouteListNode first;
@@ -9,4 +11,20 @@ public class RouteList
                 first,
                 location);
     }
+
+    public void addLast(Location location)
+    {
+        RouteListNode last = first;
+        while ( last.getNext() != null )
+        { last = last.getNext();  }
+        RouteListNode newNode
+                = new RouteListNode(null, location);
+        last.setNext(newNode);
+    }
+    public Location get(int index)
+    {
+
+    }
+
+
 }
