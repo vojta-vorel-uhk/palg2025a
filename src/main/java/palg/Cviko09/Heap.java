348fb96c6c6f75df7c8061df66de19b5f7ae0955
Vojta Vorel <vorelvo2@uhk.cz>
2025-11-20 11:38:18 +0100

    Během cvika 9

--- src/main/java/palg/Cviko09/Heap.java
+++ src/main/java/palg/Cviko09/Heap.java
@@ -12,18 +12,22 @@ public class Heap
         this.data = new Message[capacity];
         count = 0;
     }
-    public void add(Message m)
-    {
+    public void add(Message m){
         this.data[count] = m;
         count++;
         repair(count);
     }
     private void repair(int index){
         int parent= parent(index);
-        if(data[parent] > data[index] ){
+        if(data[parent].compareTo(data[index]) > 0){
             swap(index,parent);
             repair(parent);
         }
+    }
+    private void swap(int a, int b){
+        var temp = data[b];
+        data[b] = data[a];
+        data[a] = temp;
     }
 
     private static int leftChild(int index)
